0bbd04541996a6b14dc6d5fa31967a36d493230a
tjoeunis205 <fkrdbs1278@gmail.com>
2026-08-25 16:48:28 +0900

    Initial project setup

---
+++ src/test/java/com/young04/lastproject/LastprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young04.lastproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
