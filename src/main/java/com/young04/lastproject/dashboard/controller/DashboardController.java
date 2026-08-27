3e13953abe346f92f78effd9364b019fee55b769
tjoeunis205 <knw9526@gmail.com>
2026-08-27 12:02:40 +0900

    관리자 데시보드 페이지만 완(임시)

--- src/main/java/com/young04/lastproject/dashboard/controller/DashboardController.java
+++ src/main/java/com/young04/lastproject/dashboard/controller/DashboardController.java
@@ -1,4 +1,14 @@
 package com.young04.lastproject.dashboard.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DashboardController {
+
+    @GetMapping("/admin/dashboard")
+    public String dashboard() {
+
+        return "admin/dashboard";
+    }
 }
